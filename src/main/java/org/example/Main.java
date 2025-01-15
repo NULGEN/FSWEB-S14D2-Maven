f3233196747d1bb05e883d9af31d7abe564ecf71
Nilufer Ulgen <nilufer.korkmaz@gmail.com>
2025-01-15 11:42:14 +0300

    "Id: 194778" proje çözümü

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Bedroom bedroom = new Bedroom("salon",
+//                new Wall("doğu"), new Wall("kuzey"),new Wall("batı"), new Wall("güney"),
+//                new Ceiling(5, PaintColor.WHITE),
+//                new Bed("soft",2,3,1,4),
+//                new Lamp(LampType.LAVA,true,80),
+//                new Wardrobe(3,2,20),
+//                new Carpet(2,10, PaintColor.RED));
+//
+//        System.out.println(bedroom.toString());
     }
 }
---
+++ src/main/java/org/example/model/Carpet.java
@@ -0,0 +1,30 @@
+package org.example.model;
+
+import org.example.model.enums.PaintColor;
+
+public class Carpet {
+ private int height;
+ private  int width;
+ private PaintColor color;
+
+    public Carpet(int height, int width, PaintColor color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public PaintColor getColor() {
+        return color;
+    }
+    public  void lying(){
+        System.out.println("Carpet is lying on Bedroom floor.");
+    }
+}
